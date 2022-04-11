c96d9cd2313c9989fce0de683b45231b10506532
Luca <lucavaccari03@gmail.com>
2022-04-11 13:11:10 +0200

    Realtime database create and read

--- public/js/pazienti.js
+++ public/js/pazienti.js
@@ -1,11 +1,7 @@
 import * as config from './config.js';
-import { getDatabase, ref, onValue } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js';
+import { getPazienti } from './realtimeInterface.js';
 
 if (config.DB_SYSTEM == 'realtime') {
-    const db = getDatabase();
-    onValue(ref(db, '/'), snapshot => {
-        $("#pazienti_list").text((JSON.stringify(snapshot.val())));
-    });
 }
 else if (config.DB_SYSTEM == "firestore") {
 
---
+++ public/js/realtimeInterface.js
@@ -0,0 +1,70 @@
+import { getDatabase, ref, child, get, set } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js';
+
+// CREATE
+export function addOnDb(path, value) {
+    const postRef = push(ref(getDatabase(), path));
+    set(postRef, value);
+};
+
+export function addOnDb(path, value, id) {
+    set(ref(getDatabase(), path + '/' + id), value);
+};
+
+export function addPaziente(paziente) {
+    if (paziente.cf_paziente == undefined)
+        console.error("CF paziente obbligatorio");
+    else
+        addOnDb('/pazienti', paziente, paziente.cf_paziente);
+}
+
+export function addMedico(medico) {
+    if (medico.cf_medico == undefined)
+        console.error("CF medico obbligatorio");
+    else
+        addOnDb('/medico', medico, medico.cf_medico);
+};
+
+export function addCartella(cartella, cf_paziente) {
+    addOnDb('/cartelle/' + cf_paziente, cartella);
+}
+// END CREATE
+
+// READ
+export async function getFromDb(path) {
+    let snapshot = await get(child(ref(getDatabase()), path));
+    if (snapshot.exists())
+        return snapshot.val();
+    else
+        return {};
+}
+
+export async function getPazienti() {
+    return await getFromDb('/pazienti');
+}
+
+export async function getPaziente(cf_paziente) {
+    return await getPazienti()[cf_paziente];
+}
+
+export async function getMedici() {
+    return await getFromDb('/medici');
+}
+
+export async function getMedico(cf_medico) {
+    return await getMedici()[cf_medico];
+}
+
+export async function getCartelle(cf_paziente) {
+    return await getFromDb(`/cartelle_cliniche/${cf_paziente}`);
+}
+
+export async function getCartella(cf_paziente, id_cartella) {
+    return await getCartella(cf_paziente)[id_cartella];
+}
+// END READ
+
+// UPDATE
+// END UPDATE
+
+// DELETE
+// END DELETE
