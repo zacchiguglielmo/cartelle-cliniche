b9fcc3adeaffe11de21b10d66bc6a2a67cbf3f36
Luca <57528364+LucaVaccari@users.noreply.github.com>
2022-04-11 11:10:46 +0200

    Delete rest-server

--- rest-server/index.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import 'dotenv/config';
-const app = express();
-
-app.listen(process.env.SERVER_PORT, () => {
-    console.log("Server listening on " + process.env.SERVER_PORT);
-});
