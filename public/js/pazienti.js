c1cd9824f7f431428b0db46802e670a5a232a5ec
Luca <lucavaccari03@gmail.com>
2022-04-18 10:20:53 +0200

    Show pazienti (realtime)

--- public/js/pazienti.js
+++ public/js/pazienti.js
@@ -1,11 +1,57 @@
-import * as config from './config.js';
-import { getPazienti } from './realtimeInterface.js';
+import * as config from "./config.js";
+import { getPazienti } from "./realtimeInterface.js";
 
-if (config.DB_SYSTEM == 'realtime') {
+function getAge(dateString) {
+  var today = new Date();
+  var birthDate = new Date(dateString);
+  var age = today.getFullYear() - birthDate.getFullYear();
+  var m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age;
 }
-else if (config.DB_SYSTEM == "firestore") {
 
-}
-else {
+async function loadPazienti() {
+  let pazienti;
+  if (config.DB_SYSTEM == "realtime") {
+    pazienti = await getPazienti();
+  } else if (config.DB_SYSTEM == "firestore") {
+  } else {
     console.error("Unknown db system: " + config.DB_SYSTEM);
+    return;
+  }
+
+  let response = await fetch("templates/templatePaziente.html");
+  let text = await response.text();
+  for (let i in pazienti) {
+    let pazienteElement = document.createElement("div");
+    pazienteElement.innerHTML = text;
+    pazienteElement.setAttribute("class", "row");
+
+    // SET VALUES
+    pazienteElement.querySelector("#nome").innerHTML = pazienti[i].nome;
+    pazienteElement.querySelector("#cognome").innerHTML = pazienti[i].cognome;
+    pazienteElement.querySelector("#dataNascita").innerHTML =
+      pazienti[i].dataNascita;
+    // TODO: calculate data nascita
+    pazienteElement.querySelector("#eta").innerHTML = getAge(
+      pazienti[i].dataNascita
+    );
+    pazienteElement.querySelector("#sesso").innerHTML = pazienti[i].sesso;
+
+    // FIX ID NAMES
+    pazienteElement.querySelector("#nome").setAttribute("id", "nome" + i);
+    pazienteElement.querySelector("#cognome").setAttribute("id", "cognome" + i);
+    pazienteElement
+      .querySelector("#dataNascita")
+      .setAttribute("id", "dataNascita" + i);
+    pazienteElement.querySelector("#eta").setAttribute("id", "eta" + i);
+    pazienteElement.querySelector("#sesso").setAttribute("id", "sesso" + i);
+
+    // ADD CHILD
+    document.getElementById("pazientiContainer").appendChild(pazienteElement);
+  }
 }
+
+loadPazienti();
--- public/js/realtimeInterface.js
+++ public/js/realtimeInterface.js
@@ -1,65 +1,66 @@
-import { getDatabase, ref, child, get, set } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js';
+import {
+  getDatabase,
+  ref,
+  child,
+  get,
+  set,
+} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js";
 
 // CREATE
-export function addOnDb(path, value) {
+export function addOnDb(path, value, id) {
+  if (id == undefined) {
     const postRef = push(ref(getDatabase(), path));
     set(postRef, value);
-};
-
-export function addOnDb(path, value, id) {
-    set(ref(getDatabase(), path + '/' + id), value);
-};
+  } else {
+    set(ref(getDatabase(), path + "/" + id), value);
+  }
+}
 
 export function addPaziente(paziente) {
-    if (paziente.cf_paziente == undefined)
-        console.error("CF paziente obbligatorio");
-    else
-        addOnDb('/pazienti', paziente, paziente.cf_paziente);
+  if (paziente.cf_paziente == undefined)
+    console.error("CF paziente obbligatorio");
+  else addOnDb("/pazienti", paziente, paziente.cf_paziente);
 }
 
 export function addMedico(medico) {
-    if (medico.cf_medico == undefined)
-        console.error("CF medico obbligatorio");
-    else
-        addOnDb('/medico', medico, medico.cf_medico);
-};
+  if (medico.cf_medico == undefined) console.error("CF medico obbligatorio");
+  else addOnDb("/medico", medico, medico.cf_medico);
+}
 
 export function addCartella(cartella, cf_paziente) {
-    addOnDb('/cartelle/' + cf_paziente, cartella);
+  addOnDb("/cartelle/" + cf_paziente, cartella);
 }
 // END CREATE
 
 // READ
 export async function getFromDb(path) {
-    let snapshot = await get(child(ref(getDatabase()), path));
-    if (snapshot.exists())
-        return snapshot.val();
-    else
-        return {};
+  let snapshot = await get(child(ref(getDatabase()), path));
+  if (snapshot.exists()) return snapshot.val();
+  else return {};
 }
 
 export async function getPazienti() {
-    return await getFromDb('/pazienti');
+  return await getFromDb("/pazienti");
 }
 
 export async function getPaziente(cf_paziente) {
-    return await getPazienti()[cf_paziente];
+  return await getPazienti()[cf_paziente];
 }
 
 export async function getMedici() {
-    return await getFromDb('/medici');
+  return await getFromDb("/medici");
 }
 
 export async function getMedico(cf_medico) {
-    return await getMedici()[cf_medico];
+  return await getMedici()[cf_medico];
 }
 
 export async function getCartelle(cf_paziente) {
-    return await getFromDb(`/cartelle_cliniche/${cf_paziente}`);
+  return await getFromDb(`/cartelle_cliniche/${cf_paziente}`);
 }
 
 export async function getCartella(cf_paziente, id_cartella) {
-    return await getCartella(cf_paziente)[id_cartella];
+  return await getCartella(cf_paziente)[id_cartella];
 }
 // END READ
 
@@ -69,14 +70,14 @@ export async function getCartella(cf_paziente, id_cartella) {
 
 // DELETE
 export function deletePaziente(cf_paziente) {
-    set(ref(getDatabase(), 'pazienti/' + cf_paziente), null);
+  set(ref(getDatabase(), "pazienti/" + cf_paziente), null);
 }
 
 export function deleteMedico(cf_medico) {
-    set(ref(getDatabase(), 'medici/' + cf_medico), null);
+  set(ref(getDatabase(), "medici/" + cf_medico), null);
 }
 
-export function deletePaziente(cf_paziente, id_cartella) {
-    set(ref(getDatabase(), 'pazienti/' + cf_paziente + "/" + id_cartella), null);
+export function deleteCartella(cf_paziente, id_cartella) {
+  set(ref(getDatabase(), "pazienti/" + cf_paziente + "/" + id_cartella), null);
 }
 // END DELETE
